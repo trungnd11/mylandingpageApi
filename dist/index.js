322244f19156c61143cea5bb1997035fde110d5c
trungnd <trungnd3@vetc.com.vn>
2022-09-01 11:04:25 +0700

    Fix pỏtt

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 8085;
 app.get("/", (req, res) => {
-    res.send("The sedulous hyena ate the antelope!");
+    res.send("The sedulous hyena ate the antelope!!!!");
 });
 app.listen(port, () => {
     return console.log(`server is listening on ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
 
 const app = express();
 const port = 8085;
+
+app.use(cors());
+app.use(bodyParser.json({ limit: "10mb" }));
 
 app.get("/", (req, res) => {
   res.send("The sedulous hyena ate the antelope!");
